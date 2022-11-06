e164c8cfe878943c73c17e26d7b69bb93cae7333
Alex <al-an2017@rambler.ru>
2022-11-06 18:07:27 +0300

    Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,19 @@
+﻿// Написать цикл, который принимает на вход два числа (A и B) и возводит 
+// число A в натуральную степень B.
+Console.WriteLine("Введите число A: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите натуральное число B: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+if (numberB == 0)
+{
+Console.WriteLine("А в степени В равно: 1");
+}
+else
+{
+int result = numberA;
+for (int i = 1; i < numberB; i++)
+{
+result = result * numberA;
+}
+Console.WriteLine("A в степени B равно: " + result);
+}
