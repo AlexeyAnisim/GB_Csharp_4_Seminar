ffdb55fc1b0203a307a4e3b5612150300409e834
Alex <al-an2017@rambler.ru>
2022-11-06 20:41:38 +0300

    Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,13 @@
+﻿// Программа, которая задаёт массив из 8 элементов и выводит их на экран.
+int[] arr = {5, 6, 3, 2, 2, 6, 7, 1, 0};
+
+void PrintArray(int[] array)
+{
+int count = array.Length;
+for (int i = 0; i < count; i++)
+{
+Console.Write($"{array[i]} ");
+}
+Console.WriteLine();
+}
+PrintArray(arr);
